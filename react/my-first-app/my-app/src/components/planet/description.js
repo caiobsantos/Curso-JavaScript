0dfa99d28b0d1e76ba5e7e0fd248be08c31d23b8
Caio Santos <caiobs10@gmail.com>
2023-02-28 14:42:54 -0300

    renderização condicional

--- react/my-first-app/my-app/src/components/planet/description.js
+++ react/my-first-app/my-app/src/components/planet/description.js
@@ -1,10 +1,20 @@
 const PlanetDescription = (props) => {
-    return (
-        <div>
-            <p>{props.description}</p>
-            <a href={props.link}>Link</a>
-        </div>
-    )
+    if(props.link){
+        return (
+            <div>
+                <p>{props.description}</p>
+                <a href={props.link}>Link</a>
+            </div>
+        )
+    }
+    else{
+        return (
+            <div>
+                <p>{props.description}</p>
+                <p>Não há link</p>
+            </div>
+        )
+    }
 }
 
 export default PlanetDescription;
